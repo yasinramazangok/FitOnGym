c6a480eb82f3587734a91e1bb93e6858e551b5c9
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2024-09-28 23:47:07 +0300

    Project created. Templates and tiers added.

---
+++ FitOnWebSite/FitOnWebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FitOnWebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
